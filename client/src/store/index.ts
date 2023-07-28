63c49546f617b56dccbc0dee2b41fb2691cf4427
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-07-28 15:29:52 +0300

    added fileReducer

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -2,9 +2,11 @@ import { combineReducers, legacy_createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { userReducer } from "./reducers/userReducer";
+import { fileReducer } from "./reducers/fileReducer";
 
 const root = combineReducers({
   user: userReducer,
+  file: fileReducer,
 });
 
 export type RootStore = ReturnType<typeof root>
