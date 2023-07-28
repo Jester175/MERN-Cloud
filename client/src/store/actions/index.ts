d79f6ac60b334af5b2232ff2bcaba5252e778a03
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-07-28 15:30:07 +0300

    export some actions

--- client/src/store/actions/index.ts
+++ client/src/store/actions/index.ts
@@ -1,7 +1,10 @@
 import { registration, auth, login } from "./user";
+import { getFiles, createDir } from "./file";
 
 export default {
   registration,
   login,
   auth,
+  getFiles,
+  createDir,
 };
