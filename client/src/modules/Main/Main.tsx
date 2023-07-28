cd321cd94e14778abece891c4a04bcf7b5acc74b
Nikita Kolesnew <chattrain.kol7@gmail.com>
2023-07-28 15:27:52 +0300

    created main page

--- client/src/modules/Main/Main.tsx
+++ client/src/modules/Main/Main.tsx
@@ -1,12 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { setCurrentDir } from "../../store/reducers/fileReducer";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+
+import { ArrowRight } from "../../components/icons/ArrowRight";
+import { FileList } from "./FileList";
+import { Popup } from "./Popup";
 
 import styles from "./main.module.css";
 
 export const Main: React.FC = () => {
+  const dispatch = useDispatch();
+  const { getFiles } = useActions();
+  const { currentDir, dirStack } = useTypedSelector((state) => state.file);
+  const [isPopup, setIsPopup] = useState<boolean>(false);
+
+  useEffect(() => {
+    getFiles(currentDir);
+  }, [currentDir]);
+
+  const handleTogglePopup = () => {
+    setIsPopup(!isPopup);
+  };
+
+  const handleBackClick = () => {
+    const backDirId = dirStack.pop();
+    dispatch(setCurrentDir(backDirId));
+  };
+
   return (
-    <div>
-      <Link to="/registration">Login</Link>
+    <div className={styles.disk}>
+      <div className={styles.disk__btns}>
+        <button
+          className={`${styles.disk__back} ${styles.disk__control}`}
+          onClick={handleBackClick}
+        >
+          <ArrowRight />
+          Back
+        </button>
+        <button
+          className={`${styles.disk__create} ${styles.disk__control}`}
+          onClick={handleTogglePopup}
+        >
+          Create folder
+        </button>
+      </div>
+      <FileList />
+      {isPopup && <Popup onClose={handleTogglePopup} />}
     </div>
   );
 };
